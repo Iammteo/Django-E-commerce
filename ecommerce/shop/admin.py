36ffada1419459d17384d46cc4a793e422c0010a
Freda Bazuaye <fredabazuaye@gmail.com>
2025-12-03 21:26:14 +0000

    Updated Admin

--- ecommerce/shop/admin.py
+++ ecommerce/shop/admin.py
@@ -1,7 +1,37 @@
+# from django.contrib import admin
+# from .models import Product
+
+
+from .models import Order
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
 from django.contrib import admin
 from .models import Product
 
-
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "price")
+    list_display  = ("name", "category", "price", "active", "created_at")
+    list_filter   = ("category", "active")
+    search_fields = ("name", "category")
+
+
+# optional: unregister default
+admin.site.unregister(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "is_staff", "is_active", "date_joined")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    search_fields = ("username", "email")
+
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display  = ("id", "user", "product", "status", "total_price", "created_at")
+    list_filter   = ("status", "created_at")
+    search_fields = ("user__username", "product__name")
+    date_hierarchy = "created_at"
+
