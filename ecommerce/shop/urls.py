d54b96721d3500e3d0c7569a93f710aa6f5919f0
basitraj <abdulbaasitraji431@gmail.com>
2025-12-03 21:02:38 +0100

    added auth to sign up and sign in

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -1,8 +1,12 @@
 
 from django.urls import path
-from .views import home, products_page
+from .views import home, products_page, dashboard_page, register_page, signin_page
 
 urlpatterns = [
     path('', home, name='home'),
     path('products/', products_page, name='products'),
+    path('dashboards/', dashboard_page, name='dashboards'),
+    path('auth/login/', signin_page, name='signin'),
+    path('auth/register/', register_page, name='register'),
 ]
+
