f5b5c54428849df23528bda25075be15b864507a
Olatuyole <omoyeleolatuyole39@gmail.com>
2025-12-05 06:19:26 +0000

    Removed hardcoded secrets and added environment variablesss

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -72,11 +72,15 @@ MIDDLEWARE = [
 # Email settings (Gmail SMTP)
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+
+
 EMAIL_PORT = 587
+
 EMAIL_USE_TLS = True          # important for port 587
 
-EMAIL_HOST_USER = "olatuyoleomoyele40@gmail.com"     # your Gmail
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
@@ -155,3 +159,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
