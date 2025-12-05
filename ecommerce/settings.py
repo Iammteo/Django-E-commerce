69b2bf49d1b6d10b6e8b98e8bdd4456f6f256960
Olatuyole <omoyeleolatuyole39@gmail.com>
2025-12-05 06:07:33 +0000

    Removed hardcoded secrets and added environment variablesss

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,8 @@ STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")  # Optional for now
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gvbp$(j2zwbxda=wsqgjt%+h=01)pk_&$t6utoe8()r6*(hfka'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
